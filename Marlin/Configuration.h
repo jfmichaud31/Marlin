0c90c58f77d189dc9dfbcb582cb2a72ee110f040
Jean-Francois Michaud <jfmichaud31@gmail.com>
2020-03-26 21:04:29 -0400

    Updated hotend PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,9 +493,14 @@
 
   // ANET A8 Standard Extruder at 210 Degree Celsius and 100% Fan
   //(measured after M106 S255 with M303 E0 S210 C8)
-  #define DEFAULT_Kp 21.0
-  #define DEFAULT_Ki 1.25
-  #define DEFAULT_Kd 86.0
+  ////#define DEFAULT_Kp 21.0
+  //#define DEFAULT_Ki 1.25
+  //#define DEFAULT_Kd 86.0
+
+  // ANET A8 Standard Extruder with sock on and 220 Degree Celcius and 60% Fan
+  #define DEFAULT_Kp 40.06
+  #define DEFAULT_Ki 3.47
+  #define DEFAULT_Kd 115.70
 
 #endif // PIDTEMP
 
